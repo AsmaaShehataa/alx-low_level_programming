59da097b207424a665f4203c240d09a83df0622a
Asmaa Shehata <asmaa_shihata@yahoo.com>
2023-06-17 05:52:47 +0300

    two numbers

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: alphabet using putchar()
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	int i =0 , j;
+
+	while (i <= 9)
+	{
+		j = 0;
+		while (j <= 9)
+		{
+			if (i != j && i < j)
+			{
+				putchar(i + 48);
+				putchar(j + 48);
+				if (i + j != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			j++;
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
