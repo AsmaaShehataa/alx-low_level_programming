c032494e5283222a09ef2e59a97c615cb2675d42
Asmaa Shehata <asmaa_shihata@yahoo.com>
2023-06-17 04:35:37 +0300

    print from one to nine

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: printing form 0 to 9
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	int a=0;
+
+	while (a < 10)
+	{
+		printf("%d\n", a);
+		a++;
+	}
+	return (0);
+}
