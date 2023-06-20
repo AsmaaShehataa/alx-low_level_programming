31e1e9321e422620ee4375c96249426f9b6cac31
Asmaa Shehata <asmaa_shihata@yahoo.com>
2023-06-20 22:36:25 +0300

    print alphapet 10 times

---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * _putchar function to print putchar
+ * c: the character to print
+ *
+ * Return: on success (0)
+*/
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
