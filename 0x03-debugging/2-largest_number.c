13928f5fb582c0899ca27144a1a458d94314b6d2
Asmaa Shehata <asmaa_shihata@yahoo.com>
2023-06-22 16:29:33 +0300

    changes a,b,c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,11 +12,11 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a >= b && c >= b)
+	if (a > b && a > b)
 	{
 		largest = a;
 	}
-	else if (c > b && a >= b)
+	else if (b > c)
 	{
 		largest = b;
 	}
