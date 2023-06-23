4ce98634d2ff77a9926266fa1786539bfaecc92b
Asmaa Shehata <asmaa_shihata@yahoo.com>
2023-06-24 00:19:32 +0300

    aa

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_square - print squares
+ *
+ * @size: size of the square
+ * return: (0) on success
+*/
+
+void print_square(int size)
+{
+	if (size <= 0)
+		_putchar('\n');
+
+	else
+	{
+		int k, l;
+
+		for (k = 1; k < size; k++)
+		{
+			for (l = 0; l < size; l++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
