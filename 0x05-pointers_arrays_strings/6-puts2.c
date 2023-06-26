1b6d6dfeaa4f77c9c6013e476a4d380b72882981
Asmaa Shehata <asmaa_shihata@yahoo.com>
2023-06-26 23:57:23 +0300

    aa

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * puts2 - a function that prints every other character of a string
+ * @i: string
+ * Return: length
+ */
+
+void puts2(char *str)
+{
+	int num1 = 0;
+	int num2 = 0;
+	char *x = str;
+	int num3;
+
+	while (*x != '\0')
+	{
+		x++;
+		num1++;
+	}
+	num2 = num1 - 1;
+
+	for (num3 = 0; num3 <= num2; num3++)
+	{
+		if(num3 % 2 == 0)
+		{
+			_putchar(str[num3]);
+		}
+	}
+	_putchar('\n');
+}
