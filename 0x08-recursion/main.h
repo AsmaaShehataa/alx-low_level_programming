a4470033021213e3eff6139ce2ea5aace1defb70
Asmaa Shehata <asmaa_shihata@yahoo.com>
2023-07-06 00:22:02 +0300

    betty

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -12,5 +12,6 @@ int func_square_recursion(int n, int i);
 int is_prime_number(int n);
 int actual_prime(int n, int i);
 int is_palindrome(char *s);
+int check_palindrome(char *s, int i, int len);
 
 #endif
