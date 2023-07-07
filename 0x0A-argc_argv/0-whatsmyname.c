8e1c0fe2592d0b2a34d5b75645a026834aca4f3c
Asmaa Shehata <asmaa_shihata@yahoo.com>
2023-07-08 02:43:25 +0300

    aa

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: number of arguments
+ * @argv: command line arguments
+ * Return: 0 on success
+*/
+
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
