90d53bb621c5dcca249956d168dc44c6f0c6715d
Asmaa Shehata <asmaa_shihata@yahoo.com>
2023-07-11 01:04:45 +0300

    aa

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+
+
+/**
+ * *create_array - create an array with chars
+ * @size: input size
+ * @c: output size
+ * Return: always (0) on success
+*/
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+
+	char *arr = malloc(size * sizeof(char));
+	if (arr == NULL)
+		return NULL;
+
+	for (i = 0; i < size; i++)
+	{
+		arr[i] = c;
+	}
+	return (arr);
+}
