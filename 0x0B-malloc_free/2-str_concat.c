337cc38a1f6cafe2835e2616cece50dfebc6416e
Asmaa Shehata <asmaa_shihata@yahoo.com>
2023-07-11 04:44:16 +0300

    aa

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -17,6 +17,11 @@ char *str_concat(char *s1, char *s2)
 
 	conc_str = malloc(len1 + len2 + 1);
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
 	if (conc_str == NULL)
 		return (NULL);
 
