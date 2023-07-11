c5d38f787ff0fc35140ca1f72d322c1b574d424b
Asmaa Shehata <asmaa_shihata@yahoo.com>
2023-07-12 00:03:41 +0300

    aa

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -25,11 +25,17 @@ int **alloc_grid(int width, int height)
 		grid[i] = malloc(sizeof(int) * width);
 		if (grid[i] == NULL)
 			{
-			for (j = 0; j < i; j++)
-			free(grid[j]);
+			for (; i >= 0; i--)
+			free(grid[i]);
 			free(grid);
 			return (NULL);
 			}
 		}
+		for (i = 0; i < height; i++)
+		{
+			for (j = 0; j < width; j++)
+			grid[i][j] = 0;
+		}
+
 		return (grid);
 }
