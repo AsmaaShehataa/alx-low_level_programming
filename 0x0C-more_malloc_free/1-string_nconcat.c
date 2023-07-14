f8a08dc34c756479c59e5a296f45fafb4b6da2f0
Asmaa Shehata <asmaa_shihata@yahoo.com>
2023-07-14 18:55:12 +0300

    aa

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,14 +12,14 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *a;
-	unsigned int len1 = 0, len2 = 0, i, j;
+	unsigned int len1 = 0, len2 = 0, i = 0, j = 0;
 
 	/* calculate the length of the strings s1 & s2 */
 
-	while (s1[len1] != '\0')
+	while (s1 && s1[len1])
 		len1++;
 
-	while (s2[len2] != '\0')
+	while (s2 && s2[len2])
 		len2++;
 
 	if (n < len2)
@@ -27,18 +27,24 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	else
 		a = malloc((len2 + n + 1) * sizeof(char));
 
-	if (a == NULL)
+	if (!a)
 		return (NULL);
 
 	/* concate s1 */
-	for (i = 0; i < len1; i++)
+	while (i < len1)
+	{
 		a[i] = s1[i];
+		i++;
+	}
 
 	/* concate string 2 */
-	for (j = 0; j < n; j++)
-		a[i + j] = s2[j];
+	while (n < len2 && i < (len1 + n))
+		a[i++] = s2[j++];
 
-	a[i + j] = '\0';
+	while (n >= len2 && i < (len1 + len2))
+		a[i++] = s2[j++];
+
+	a[i] = '\0';
 
 	return (a);
 }
