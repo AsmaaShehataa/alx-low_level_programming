523d17de456a8894a79d32a7ba3d09a0069ab7ba
Asmaa Shehata <asmaa_shihata@yahoo.com>
2023-07-17 20:10:08 +0300

    aa

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,8 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog structure
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ * Return: dog structure
+*/
 
 typedef struct dog
 {
-	char* name; 
-	float age; 
-	char* owner;
+	char *name;
+	float age;
+	char *owner;
 
-}my_dog;
+} my_dog;
+
+
+#endif
