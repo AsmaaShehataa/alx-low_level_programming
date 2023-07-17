eaf5e863edaad1cc656b127224a987a56f40ef5a
Asmaa Shehata <asmaa_shihata@yahoo.com>
2023-07-17 20:59:35 +0300

    aa

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,20 +2,20 @@
 #define DOG_H
 
 /**
- * struct dog - dog structure
- * @name: name of dog
- * @age: age of dog
- * @owner: owner of dog
- * Return: dog structure
-*/
-
-typedef struct dog
+ * struct dog - a dog's basic info
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
+};
 
-} my_dog;
-
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif
