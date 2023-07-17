4e4631170e54e82bae42ec0e5e46cc67360cc93a
Asmaa Shehata <asmaa_shihata@yahoo.com>
2023-07-18 00:15:33 +0300

    aa

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -4,19 +4,19 @@
 
 /**
  * print_dog - prints the dog struct
- * @dog: the dog struct
+ * @d: the dog struct
 */
 
 void print_dog(struct dog *d)
 {
-	if( d == NULL )
-		return; 
+	if (d == NULL)
+		return;
 
-	if( d->name == NULL)
-		d-> name = "(nil)";
+	if (d->name == NULL)
+		d->name = "(nil)";
 
-	if( d->owner == NULL)
-		d-> name = "(nil)";
+	if (d->owner == NULL)
+		d->owner = "(nil)";
 
 	printf(" Name: %s\n Age:%2f\n Owner:%2s\n", d->name, d->age, d->owner);
 }
