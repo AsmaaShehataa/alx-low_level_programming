e78fe435cc03c45d5222bdb0e052cbc2ece4b384
Asmaa Shehata <asmaa_shihata@yahoo.com>
2023-07-19 20:58:20 +0300

    aa

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * print_name - print the name
+ *	@name: the name input variable
+ *	Return: void (0) on success
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+	
+	if (name == NULL || f == NULL)
+		return;
+	f(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,7 @@
+#ifndef _function_pointers_h
+#define _function_pointers_h
+
+void print_name(char *name, void (*f)(char *));
+int _putchar(char c);
+
+#endif
