d4b163614911b7957673286655c1fbb961333703
AsmaaShehataa <asmaa_shihata@yahoo.com>
2023-07-28 02:57:36 +0300

    aa

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,9 +1,15 @@
-	global   main
-	  extern    printf
+	section .data
+    format db 'Hello, Holberton', 10, 0  ; Null-terminated string for "Hello, Holberton" followed by a newline character
+
+section .text
+    extern printf
+
+global main
+
 main:
-	  mov   edi, format
-	  xor   eax, eax
-	  call  printf
-	  mov   eax, 0
-	  ret
-format: db 'Hello, Holberton\n',0
+    mov rdi, format  ; Load the address of the format string into the first argument (RDI)
+    xor eax, eax     ; Clear EAX (return value register) to indicate no floating-point arguments
+    call printf      ; Call the printf function
+    xor eax, eax     ; Set the return value to 0 (optional, as it is already 0 by default)
+    ret              ; Return from the main function
+
