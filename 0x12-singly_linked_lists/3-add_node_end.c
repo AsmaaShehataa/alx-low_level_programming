3cbe24d14e8535986e3a28ec378717ba8c9cce22
AsmaaShehataa <asmaa_shihata@yahoo.com>
2023-07-28 01:01:42 +0300

    from a to z

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+
+/**
+ * add_node - adds a new node in the beg of list
+ * @head: address of a pointer to a head node
+ * @str: str field of node
+ * Return: new node
+*/
+
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node = malloc(sizeof(list_t));
+	list_t *node = *head;
+
+	if (!head || !new_node)
+		return (NULL);
+
+	if (str)
+	{
+		new_node->str = strdup(str);
+		if (!new_node->str)
+		{
+			free(new_node);
+			return (NULL);
+		}
+		new_node->len = str_len(new_node->str);
+	}
+	if (node)
+	{
+		while (node->next)
+		node = node->next;
+		node->next = new_node;
+	}
+	else
+		*head = new_node;
+	return (new_node);
+}
