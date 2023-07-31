8bf06819ff57029c6c8b41756889e06093e21684
AsmaaShehataa <asmaa_shihata@yahoo.com>
2023-08-01 01:23:39 +0300

    edited task4

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,20 +1,20 @@
 	#include "lists.h"
 
 	/**
-	 * free_listint -  frees a listint_t list.
+	 * free_listint - frees the linked list form the memory
 	 * @head: pointer to the node
 	 * Return: void (0) on success
 	*/
 
 	void free_listint(listint_t *head)
 	{
-		listint_t *freeme;
+		listint_t *tempo;
 
 
 		while (head)
 		{
 			freeme = head->next;
-			free(freeme);
-			head = freeme;
+			free(head);
+			head = tempo;
 		}
 	}
