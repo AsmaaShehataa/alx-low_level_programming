99e9637ad59d1b31e417fa1cf74206bc69220155
AsmaaShehataa <asmaa_shihata@yahoo.com>
2023-08-01 03:26:18 +0300

    aa

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,7 +12,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	listint_t *mynewN = head;
 	unsigned int i = 0;
 
-	while (i && mynewN < index)
+	while (mynewN && i < index)
 	{
 		mynewN = mynewN->next;
 		i++;
