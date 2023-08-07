eebc706dd3c9a39073645292e7b95c4f0b07d601
AsmaaShehataa <asmaa_shihata@yahoo.com>
2023-08-08 02:10:08 +0300

    aa

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -14,19 +14,19 @@ int main(int ac, char **av)
 	char buf[READ_BUF_SIZE];
 
 	if (ac != 3)
-		dprintf(STDERR_FILENO, USAGE);
-		exit(97);
+		dprintf(STDERR_FILENO, USAGE), exit(97);
+
 	from_filedata = open(av[1], O_RDONLY);
 	if (from_filedata == -1)
-		dprintf(STDERR_FILENO, ERR_NOREAD, av[1]);
-		exit(98);
+		dprintf(STDERR_FILENO, ERR_NOREAD, av[1]), exit(98);
+
 	to_filedata = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, PERMISSIONS);
 	if (to_filedata == -1)
 	dprintf(STDERR_FILENO, ERR_NOWRITE, av[2]), exit(99);
+
 	while ((bb = read(from_filedata, buf, READ_BUF_SIZE)) > 0)
 		if (write(to_filedata, buf, bb) != bb)
-			dprintf(STDERR_FILENO, ERR_NOWRITE, av[2]);
-			exit(99);
+			dprintf(STDERR_FILENO, ERR_NOWRITE, av[2]), exit(99);
 
 	if (bb == -1)
 		dprintf(STDERR_FILENO, ERR_NOREAD, av[1]);
