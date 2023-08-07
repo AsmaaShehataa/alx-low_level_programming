7eaf8af92ca4ede1d275d4993adb4be5b46fff14
AsmaaShehataa <asmaa_shihata@yahoo.com>
2023-08-08 01:45:37 +0300

    moving a tested file of C to TF

---
+++ 0x15-file_io/Test_folder/3-cp.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+#define USAGE "Usage: cp file_from file_to\n"
+#define ERR_NOREAD "Error: Can't read from file %s\n"
+#define ERR_NOWRITE "Error: Can't write from file %s\n"
+#define ERR_NOCLOSE "Error: Can't close fd %d\n"
+#define PERMISSIONS (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH)
+#define READ_BUF_SIZE 1024
+
+
+/**
+ * main - program
+ * @ac: args count
+ * @av: args vector
+ * Return: 1 on success 0 on failure
+*/
+
+int main(int ac, char **av)
+{
+	int from_filedata = 0, to_filedata = 0;
+	ssize_t bb;
+	char buf[READ_BUF_SIZE];
+
+	if (ac != 3)
+		dprintf(STDERR_FILENO, USAGE), exit(97);
+
+	from_filedata = open(av[1], O_RDONLY);
+	if (from_filedata == -1)
+		dprintf(STDERR_FILENO, ERR_NOREAD, av[1]), exit(98);
+
+	to_filedata = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, PERMISSIONS);
+	if (to_filedata == -1)
+	dprintf(STDERR_FILENO, ERR_NOWRITE, av[2]), exit(99);
+
+	while ((bb = read(from_filedata, buf, READ_BUF_SIZE)) > 0)
+		if (write(to_filedata, buf, bb) != bb)
+			dprintf(STDERR_FILENO, ERR_NOWRITE, av[2]), exit(99);
+
+	if (bb == -1)
+		dprintf(STDERR_FILENO, ERR_NOREAD, av[1]), exit(98);
+
+	from_filedata = close(from_filedata);
+	to_filedata = close(to_filedata);
+
+	if (from_filedata)
+		dprintf(STDERR_FILENO, ERR_NOCLOSE, from_filedata), exit(100);
+
+	if (to_filedata)
+		dprintf(STDERR_FILENO, ERR_NOCLOSE, from_filedata), exit(100);
+
+	return (0);
+}
