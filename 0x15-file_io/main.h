6b6b1cfde1b31449158e6e15a2d2567c6c43a34d
Asmaa Shehata <asmaa_shihata@yahoo.com>
2023-08-25 14:21:57 -0700

    enhanced file of 100-elf_header

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -19,5 +19,16 @@ int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 char *create_buffer(char *file);
 void close_file(int fd);
+void checking_elf(unsigned char *ent_ident);
+void printing_magic(unsigned char *ent_ident);
+void printing_class(unsigned char *ent_ident);
+void printing_data(unsigned char *ent_ident);
+void print_elf_version(unsigned char *ent_ident);
+void print_My_osabi(unsigned char *ent_ident);
+void print_my_elf_abi(unsigned char *ent_ident);
+void print_elf_header_type(unsigned int e_type, unsigned char *ent_ident);
+void print_elfheader_entry(unsigned long int e_entry, unsigned char *ent_ident);
+void close_elf(int elf);
+int main(int __attribute__((__unused__)) argc, char *argv[]);
 
 #endif
