4873860d50c5ec2e79807dcf0b7a51e168e4b351
AsmaaShehataa <asmaa_shihata@yahoo.com>
2023-09-14 18:19:26 +0300

    new and end nodes

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint_end - function that adds a new node at the end
+ * @head: linked list to be returned
+ * @n: number of nodes
+ * Return: a new node at the beginning
+*/
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+dlistint_t *new_node;
+dlistint_t *end_node;
+int num = n;
+
+new_node = malloc(sizeof(dlistint_t));
+if (!end_node)
+return (NULL);
+
+new_node->n = num;
+new_node->next = NULL;
+
+if (*head == NULL)
+{
+new_node->prev = NULL;
+*head = new_node;
+return (new_node);
+}
+end_node = *head;
+while (end_node->next != NULL)
+end_node = end_node->next;
+end_node->next = new_node;
+new_node->prev = end_node;
+
+return (new_node);
+}
