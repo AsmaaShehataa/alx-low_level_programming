b5b85b237c3c97e569867837d017167c0dae83b3
Asmaa Shehata <asmaa_shihata@yahoo.com>
2023-10-06 14:12:26 -0700

    aa

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,13 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - function gives the index of a key
+ * @key: Input variable
+ * size: size of the array
+ * Return: The index of the key
+*/
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djb2(key) % size);
+}
