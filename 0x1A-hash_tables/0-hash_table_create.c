5c06e14131b5589546a848d3384fe09b8ea09dd9
Asmaa Shehata <asmaa_shihata@yahoo.com>
2023-10-06 12:48:02 -0700

    creation a pointer to the returned hash table

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - function create a hash table
+ * @size: input variable
+ *
+ * Return: Null with an error otherwise pointer to the new hash_tble
+*/
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *ht;
+	unsigned long int i;
+
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
+		return (NULL);
+
+	ht->size = size;
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+	if (ht->array == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		ht->array[i] = NULL;
+	return (ht);
+}
